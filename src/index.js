4c9eebde09f066831a191c11035817ab1e3ae939
Alex <askovalev93@gmail.com>
2023-01-25 13:50:58 +1000

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = function reverse (n) {
-  
+    return Math.sign(n) === -1 ? n.toString().slice(1, n.length).split("").reverse().join("") : n.toString().split("").reverse().join("");
 }
